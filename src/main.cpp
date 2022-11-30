baf34c6478f098a909ebd7944d88c7f76f5d3c2b
pchchv <ipchchv@gmail.com>
2022-11-30 23:53:30 +0300

    Implement a main func

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include "hashtable.hpp"
+#include "customer.hpp"
+#include "BST_tree.hpp"
+#include "admin.hpp"
+#include "staff.hpp"
+#include "string.h"
+
+void boot()
+{
+    Hashtable H;
+    BST_Tree T;
+    H.starthash();
+    T.load_Server();
+}
+
+int main()
+{
+    void boot();
+
+    int condition = 0;
+    while (condition != 4)
+    {
+        cout << "YOU want to login as:\t\t\t\t\t\t" << endl
+             << endl;
+        cout << "1-ADMIN" << endl;
+        cout << "2-STAFF" << endl;
+        cout << "3-CUSTOMER" << endl;
+        cin >> condition;
+        if (condition == 1)
+        {
+            admin();
+        }
+        if (condition == 2)
+        {
+        }
+        if (condition == 3)
+        {
+            customer();
+        }
+        if (condition == 4)
+        {
+            condition = 4;
+        }
+    }
+
+    system("pause");
+    return 0;
+}
